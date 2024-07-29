ed7eeb8ca481260564a4caff63bc4b2afc0fed63
Megan Hilbert <meganmhilbert@gmail.com>
2024-07-29 17:09:59 -0500

    completed contact page, updated footer.

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,10 +1,77 @@
-import { Outlet } from "react-router-dom"
+import { useState } from "react"
 
 const Contact = () => {
+  
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+  const [error, setError] = useState('')
+
+  const handleNameBlur = (e) => {
+    if (!name) {
+      setError('Name is required.')
+    }
+  }
+
+  const handleEmailValidation = (e) => {
+    const isValidEmailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+
+    if (!email) {
+      setError('E-mail is required.')
+    } else if (!email.match(isValidEmailPattern)) {
+      setError('Please enter a valid e-mail.')
+    }
+  }
+
+  const handleMessageBlur = (e) => {
+    if (!message) {
+      setError('Message is required.')
+    }
+  }
+  
   return (
     <>
       <h1>Contact Me</h1>
-      <Outlet />
+      <div className="w-1/2 flex flex-col gap-6">
+        <label className="input input-bordered flex items-center gap-2">
+          Name: 
+          <input 
+            value={name}
+            onChange={(e) => {setName(e.target.value)}}
+            onBlur={handleNameBlur}
+            type="text" 
+            className="grow" 
+            placeholder="Your Name"
+           />
+        </label>
+        <label className="input input-bordered flex items-center gap-2">
+          Email
+          <input
+            value={email}
+            onChange={(e) => {setEmail(e.target.value)}}
+            onBlur={handleEmailValidation}
+            type="text" 
+            className="grow" 
+            placeholder="your@email.com" 
+          />
+        </label>
+        <div className="flex">
+          <textarea 
+            className="textarea textarea-bordered grow" 
+            placeholder="Message"
+            onChange={(e) => {setMessage(e.target.value)}}
+            onBlur={handleMessageBlur}
+          >
+            {message}
+
+          </textarea>
+
+        </div>
+          { error && <p className="text-red-400">{error}</p>}
+          <button className="btn btn-active btn-secondary">Send</button>
+      </div>
+
+
     </>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,22 @@
 const Footer = () => {
     return (
 
-        <footer className="p-8 text-center bg-petal mt-8">
-            © Megan Hilbert 2024 ❤️
+        <footer className="flex justify-center gap-8 p-8 text-center bg-petal mt-8">
+            <a 
+            className="hover:line-through" 
+            href="https://github.com/mmhilbert">
+                Github
+            </a>
+            <a 
+            className="hover:line-through" 
+            href="https://www.linkedin.com/in/megan-hilbert-ms-rdn-9962b5122/">
+                LinkedIn
+            </a>
+            <a 
+            className="hover:line-through" 
+            href="https://stackexchange.com/users/32479714/user25233870">
+                Stack Overflow
+            </a>
         </footer>
     )
 }
